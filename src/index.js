a57b459340271e778747b58000afef9ec9a916a6
Ken <kensong96@gmail.com>
2019-12-06 23:50:51 -0500

    interceptor added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import Axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+//whole axios share one configuration, can send request with auth information
+Axios.interceptors.request.use(
+    req => {
+        console.log(req);
+
+        return req; //need to return the req to unblock the request
+    },
+    err => {
+        console.log(err);
+        return Promise.reject(err); //can handle no internet or no authentication
+    }
+);
+
+Axios.interceptors.response.use(
+    res => {
+        console.log(res);
+
+        return res; //need to return the res to unblock the request
+    },
+    err => {
+        console.log(err);
+        return Promise.reject(err); //can handle no internet or no authentication
+    }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
